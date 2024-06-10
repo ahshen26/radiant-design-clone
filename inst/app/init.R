c17ea0c624c1978cb1382fbef92cf32668fd6633
ahshen26 <77656572+ahshen26@users.noreply.github.com>
2024-06-10 12:40:41 -0500

    Update init.R
    removed the design tab entirely

--- inst/app/init.R
+++ inst/app/init.R
@@ -1,31 +1,15 @@
 # import_fs("radiant.design", libs = "mvtnorm", incl = "pmvnorm")
 
-## urls for menu
+## Remove the URLs for the Design tab
 r_url_list <- getOption("radiant.url.list")
-r_url_list[["Random sampling"]] <- "design/sampling/"
-r_url_list[["Random assignment"]] <- "design/randomize/"
-r_url_list[["Sample size (single)"]] <- "design/sample-size/"
-r_url_list[["Sample size (compare)"]] <- "design/sample-size-comp/"
-r_url_list[["Design of Experiments"]] <- "design/doe/"
+r_url_list[["Random sampling"]] <- NULL
+r_url_list[["Random assignment"]] <- NULL
+r_url_list[["Sample size (single)"]] <- NULL
+r_url_list[["Sample size (compare)"]] <- NULL
+r_url_list[["Design of Experiments"]] <- NULL
 options(radiant.url.list = r_url_list)
 rm(r_url_list)
 
-## design menu
-options(
-  radiant.design_ui =
-    tagList(
-      navbarMenu(
-        "Design",
-        tags$head(
-          tags$script(src = "www_design/js/run_return.js")
-        ),
-        "DOE",
-        tabPanel("Design of Experiments", uiOutput("doe")),
-        "----", "Sample",
-        tabPanel("Random sampling", uiOutput("sampling")),
-        tabPanel("Random assignment", uiOutput("randomizer")),
-        tabPanel("Sample size (single)", uiOutput("sample_size")),
-        tabPanel("Sample size (compare)", uiOutput("sample_size_comp"))
-      )
-    )
-)
+## Remove the Design menu
+options(radiant.design_ui = NULL)
+
